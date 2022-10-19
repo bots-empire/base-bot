acf170a3c76a4b60ce9c0e75b7bb50ecb5c30c3a
Stepan1328 <stepansamsonov1337@gmail.com>
2022-10-19 17:32:49 +0300

    fix psql)))

--- mailing/mailing.go
+++ mailing/mailing.go
@@ -53,9 +53,9 @@ func (s *Service) getUsersWithMailing() ([]*MailingUser, error) {
 	rows, err := s.messages.Sender.GetDataBase().Query(`
 SELECT id, lang, advert_channel
 	FROM shazam.users
-WHERE status = $1 OR status = ''
+WHERE status = ? OR status = ''
 ORDER BY id
-	LIMIT $2;`,
+	LIMIT ?;`,
 		statusNeedMailing,
 		s.usersPerIteration)
 	if err != nil {
@@ -124,9 +124,9 @@ func (s *Service) StartMailing(channels []int) error {
 func (s *Service) markMailingUsers(usersChan int) error {
 	_, err := s.messages.Sender.GetDataBase().Exec(`
 UPDATE shazam.users 
-	SET status = $1 
-WHERE status = $2
-	AND advert_channel = $3;`,
+	SET status = ? 
+WHERE status = ?
+	AND advert_channel = ?;`,
 		statusNeedMailing,
 		statusActive,
 		usersChan)
@@ -196,8 +196,8 @@ func (s *Service) sendMailToUser(wg *sync.WaitGroup, user *MailingUser) {
 func (s *Service) markReadyMailingUser(userID int64) error {
 	_, err := s.messages.Sender.GetDataBase().Exec(`
 UPDATE shazam.users 
-	SET status = $1 
-WHERE id = $2;`,
+	SET status = ? 
+WHERE id = ?;`,
 		statusActive,
 		userID)
 	if err != nil {
